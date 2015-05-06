9ce5904baf68be2febf3198c19497ebd786decc8
Bhanu <bhanu@servelots.com>
2015-05-06 18:33:01 +0530

    removed Facebook script, keeps running async calls,

--- mouchak/static/js/views.js
+++ mouchak/static/js/views.js
@@ -13,15 +13,8 @@
       var template =  _.template($("#text-template").html());
       $(el).append(this.el);
       var tags = this.model.toJSON().tags; 
-      console.log(tags);
       $(this.el).append(template(this.model.toJSON()));
       M.appendAttrs(this.model, this.el);
-      /*$(el).append(this.el);
-      var title = this.model.get('title') || '';
-      var str = '<h4>'+ title +'</h4>'+'<p>' +
-            this.model.get('data') + '</p>'+'<span class="post-tag-foot col-lg-offset-3 col-md-offset-3 col-xs-offset-3 col-sm-offset-3">'+ 'This Article is Tagged: '+this.model.get('tags') + '</span>';
-      $(this.el).html(str);
-      M.appendAttrs(this.model, this.el);*/
     }
   });
 
